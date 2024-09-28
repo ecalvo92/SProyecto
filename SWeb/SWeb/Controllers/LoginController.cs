52d05035175f19d62c6e3e0a2e1c66e7547cfcaf
ecalvo92 <ecalvo@crnova.com>
2024-09-28 11:09:36 -0600

    Fin de semana 2

---
+++ SWeb/SWeb/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        public IActionResult CrearCuenta()
+        {
+            return View();
+        }
+
+        public IActionResult RecuperarAcceso()
+        {
+            return View();
+        }        
+
+    }
+}
